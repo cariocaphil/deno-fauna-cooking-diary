ec8f297cbe264f221434809a29fbcefce7ee6644
cariocaphil <cariocaphil@gmail.com>
2023-07-27 19:06:19 +0200

    add signup form

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,28 +5,34 @@
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/greet/[name].tsx";
-import * as $4 from "./routes/index.tsx";
-import * as $5 from "./routes/posts/[id]/edit.tsx";
-import * as $6 from "./routes/posts/[id]/index.tsx";
-import * as $7 from "./routes/posts/new.tsx";
+import * as $3 from "./routes/api/register.ts";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/index.tsx";
+import * as $6 from "./routes/posts/[id]/edit.tsx";
+import * as $7 from "./routes/posts/[id]/index.tsx";
+import * as $8 from "./routes/posts/new.tsx";
+import * as $9 from "./routes/signup.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/PostForm.tsx";
+import * as $$2 from "./islands/SignupForm.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/greet/[name].tsx": $3,
-    "./routes/index.tsx": $4,
-    "./routes/posts/[id]/edit.tsx": $5,
-    "./routes/posts/[id]/index.tsx": $6,
-    "./routes/posts/new.tsx": $7,
+    "./routes/api/register.ts": $3,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/index.tsx": $5,
+    "./routes/posts/[id]/edit.tsx": $6,
+    "./routes/posts/[id]/index.tsx": $7,
+    "./routes/posts/new.tsx": $8,
+    "./routes/signup.tsx": $9,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
     "./islands/PostForm.tsx": $$1,
+    "./islands/SignupForm.tsx": $$2,
   },
   baseUrl: import.meta.url,
 };
